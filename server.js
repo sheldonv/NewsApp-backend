0bc9560f9253f0af0adac9c833ac5cddd60fb4e9
sheldonv <velazquezsheldon@gmail.com>
2021-10-14 04:41:10 -0700

    changed back

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const https = require('https')
+const http = require('http')
 const app = require('./app')
-const server = https.createServer(app);
-server.listen( process.env.PORT || 3000, console.log('connected'))
+const server = http.createServer(app);
+server.listen( process.env.PORT || 3000, console.log(process.env.REACT_FRONTEND_URL))
 //change
